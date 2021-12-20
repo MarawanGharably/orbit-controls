e7085d2b9fc044b444c9904ab15a55a16ca17123
MarawanGharably <marawangharably@gmail.com>
2021-12-21 01:53:17 +0200

    latest

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
+
+import React, { Component } from 'react'
 import * as THREE from 'three'
 import { PointerLockControls } from './PointerLockControls'
-import React, { Component } from 'react'
+import floorImage from './images/floor2.jpg'
+import wallImage from './images/wall.jpg'
 import './App.css';
 
 const createStore = () => {
+  
+  const textureLoader = new THREE.TextureLoader();
 
   const newStore = new THREE.Group();
 
-  const wallGeometry = new THREE.PlaneGeometry(30, 10, 100, 100);
-  const wallMaterial = new THREE.MeshBasicMaterial({color : 'red', side: THREE.DoubleSide});
-  const floorGeometry = new THREE.PlaneGeometry(30, 30, 100, 100);
-  const floorMaterial = new THREE.MeshBasicMaterial({color : 'green', side: THREE.DoubleSide});
+  const wallGeometry = new THREE.PlaneGeometry(20, 5, 100, 100);
+  const wallTexture = textureLoader.load( wallImage );
+  const wallMaterial = new THREE.MeshStandardMaterial( { map: wallTexture, side: THREE.DoubleSide });
+  wallTexture.anisotropy = 16;
+  wallTexture.wrapS = wallTexture.wrapT = THREE.RepeatWrapping;
+  wallTexture.repeat.set( 1, 1 );
+  wallTexture.encoding = THREE.sRGBEncoding;
+
+  const floorGeometry = new THREE.PlaneGeometry(20, 20, 100, 100);
+  const floorTexture = textureLoader.load( floorImage );
+  const floorMaterial = new THREE.MeshStandardMaterial( { map: floorTexture, side: THREE.DoubleSide });
+  floorTexture.anisotropy = 16;
+  floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
+  floorTexture.repeat.set( 20, 20 );
+  floorTexture.encoding = THREE.sRGBEncoding;
   
   const firstWall = new THREE.Mesh( wallGeometry, wallMaterial );
-  firstWall.position.z = -15;
+  firstWall.position.z = -10;
+  firstWall.position.y = 2.5;
 
   const secondWall = new THREE.Mesh( wallGeometry, wallMaterial );
-  secondWall.position.z = 15;
+  secondWall.position.z = 10;
+  secondWall.position.y = 2.5;
 
   const thirdWall = new THREE.Mesh( wallGeometry, wallMaterial );
   thirdWall.rotation.y = Math.PI / 2;
-  thirdWall.position.x = 15;
+  thirdWall.position.x = 10;
+  thirdWall.position.y = 2.5;
 
   const fourthWall = new THREE.Mesh( wallGeometry, wallMaterial );
   fourthWall.rotation.y = Math.PI / 2;
-  fourthWall.position.x = -15;
+  fourthWall.position.x = -10;
+  fourthWall.position.y = 2.5;
 
   const floor = new THREE.Mesh( floorGeometry, floorMaterial );
   floor.rotation.x = Math.PI / 2;
@@ -109,7 +129,9 @@ export default class App extends Component {
     });
     renderer.setSize(800, 600);
 
-    const camera = new THREE.PerspectiveCamera(75, 4/3, 0.1, 1000);
+    scene.add( new THREE.AmbientLight( 0xffffff ));
+
+    const camera = new THREE.PerspectiveCamera(50, 4/3, 0.1, 1000);
     camera.position.y = 1.5;
 
     const controls = new PointerLockControls( camera, canvas );
